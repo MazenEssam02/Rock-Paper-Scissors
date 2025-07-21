9a22aec98e8de7006f1604c1745ba46694810ef1
Mazen Essam <126350118+MazenEssam02@users.noreply.github.com>
2025-07-22 00:40:23 +0300

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -16,31 +16,31 @@ if (!score) {
 }
 */
 
-let isAutoPlaying = false;
-let intervalId;
+// let isAutoPlaying = false;
+// let intervalId;
 
-//const autoPlay = () => {
+// //const autoPlay = () => {
 
-//};
-function autoPlay() {
-  if (!isAutoPlaying) {
-    intervalId = setInterval(() => {
-      const playerMove = pickComputerMove();
-      playGame(playerMove);
-    }, 1000);
-    isAutoPlaying = true;
-    document.querySelector('.js-auto-play-button').innerHTML='Stop Playing';
-  } else {
-    clearInterval(intervalId);
-    isAutoPlaying = false;
-    document.querySelector('.js-auto-play-button').innerHTML='Auto play';
-  }
-}
+// //};
+// function autoPlay() {
+//   if (!isAutoPlaying) {
+//     intervalId = setInterval(() => {
+//       const playerMove = pickComputerMove();
+//       playGame(playerMove);
+//     }, 1000);
+//     isAutoPlaying = true;
+//     document.querySelector('.js-auto-play-button').innerHTML='Stop Playing';
+//   } else {
+//     clearInterval(intervalId);
+//     isAutoPlaying = false;
+//     document.querySelector('.js-auto-play-button').innerHTML='Auto play';
+//   }
+// }
 
-document.querySelector('.js-auto-play-button')
-  .addEventListener('click',()=>{
-    autoPlay();
-  })
+// document.querySelector('.js-auto-play-button')
+//   .addEventListener('click',()=>{
+//     autoPlay();
+//   })
 
 document.querySelector('.js-rock-button')
   .addEventListener('click', () => {
